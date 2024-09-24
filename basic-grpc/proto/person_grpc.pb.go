de5c837dcbb3ef7a3b65c7f3e0c8f8c91e53f48f
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-09-23 23:31:31 -0300

    Go files generated by the protobuf and grpc plugin from the proto file

--- basic-grpc/proto/person_grpc.pb.go
+++ basic-grpc/proto/person_grpc.pb.go
@@ -22,14 +22,20 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	PersonService_Sayhello_FullMethodName = "/personService.PersonService/Sayhello"
+	PersonService_Create_FullMethodName = "/personService.PersonService/Create"
+	PersonService_Read_FullMethodName   = "/personService.PersonService/Read"
+	PersonService_Update_FullMethodName = "/personService.PersonService/Update"
+	PersonService_Delete_FullMethodName = "/personService.PersonService/Delete"
 )
 
 // PersonServiceClient is the client API for PersonService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type PersonServiceClient interface {
-	Sayhello(ctx context.Context, in *HelloReq, opts ...grpc.CallOption) (*HelloResponse, error)
+	Create(ctx context.Context, in *CreatePersonRequest, opts ...grpc.CallOption) (*PersonProfileResponse, error)
+	Read(ctx context.Context, in *SinglePersonRequest, opts ...grpc.CallOption) (*PersonProfileResponse, error)
+	Update(ctx context.Context, in *UpdatePersonRequest, opts ...grpc.CallOption) (*SuccessResponse, error)
+	Delete(ctx context.Context, in *SinglePersonRequest, opts ...grpc.CallOption) (*SuccessResponse, error)
 }
 
 type personServiceClient struct {
@@ -40,10 +46,40 @@ func NewPersonServiceClient(cc grpc.ClientConnInterface) PersonServiceClient {
 	return &personServiceClient{cc}
 }
 
-func (c *personServiceClient) Sayhello(ctx context.Context, in *HelloReq, opts ...grpc.CallOption) (*HelloResponse, error) {
+func (c *personServiceClient) Create(ctx context.Context, in *CreatePersonRequest, opts ...grpc.CallOption) (*PersonProfileResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	out := new(HelloResponse)
-	err := c.cc.Invoke(ctx, PersonService_Sayhello_FullMethodName, in, out, cOpts...)
+	out := new(PersonProfileResponse)
+	err := c.cc.Invoke(ctx, PersonService_Create_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *personServiceClient) Read(ctx context.Context, in *SinglePersonRequest, opts ...grpc.CallOption) (*PersonProfileResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(PersonProfileResponse)
+	err := c.cc.Invoke(ctx, PersonService_Read_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *personServiceClient) Update(ctx context.Context, in *UpdatePersonRequest, opts ...grpc.CallOption) (*SuccessResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(SuccessResponse)
+	err := c.cc.Invoke(ctx, PersonService_Update_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *personServiceClient) Delete(ctx context.Context, in *SinglePersonRequest, opts ...grpc.CallOption) (*SuccessResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(SuccessResponse)
+	err := c.cc.Invoke(ctx, PersonService_Delete_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
@@ -54,7 +90,10 @@ func (c *personServiceClient) Sayhello(ctx context.Context, in *HelloReq, opts .
 // All implementations must embed UnimplementedPersonServiceServer
 // for forward compatibility.
 type PersonServiceServer interface {
-	Sayhello(context.Context, *HelloReq) (*HelloResponse, error)
+	Create(context.Context, *CreatePersonRequest) (*PersonProfileResponse, error)
+	Read(context.Context, *SinglePersonRequest) (*PersonProfileResponse, error)
+	Update(context.Context, *UpdatePersonRequest) (*SuccessResponse, error)
+	Delete(context.Context, *SinglePersonRequest) (*SuccessResponse, error)
 	mustEmbedUnimplementedPersonServiceServer()
 }
 
@@ -65,8 +104,17 @@ type PersonServiceServer interface {
 // pointer dereference when methods are called.
 type UnimplementedPersonServiceServer struct{}
 
-func (UnimplementedPersonServiceServer) Sayhello(context.Context, *HelloReq) (*HelloResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Sayhello not implemented")
+func (UnimplementedPersonServiceServer) Create(context.Context, *CreatePersonRequest) (*PersonProfileResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+}
+func (UnimplementedPersonServiceServer) Read(context.Context, *SinglePersonRequest) (*PersonProfileResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Read not implemented")
+}
+func (UnimplementedPersonServiceServer) Update(context.Context, *UpdatePersonRequest) (*SuccessResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
+}
+func (UnimplementedPersonServiceServer) Delete(context.Context, *SinglePersonRequest) (*SuccessResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
 }
 func (UnimplementedPersonServiceServer) mustEmbedUnimplementedPersonServiceServer() {}
 func (UnimplementedPersonServiceServer) testEmbeddedByValue()                       {}
@@ -89,20 +137,74 @@ func RegisterPersonServiceServer(s grpc.ServiceRegistrar, srv PersonServiceServe
 	s.RegisterService(&PersonService_ServiceDesc, srv)
 }
 
-func _PersonService_Sayhello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(HelloReq)
+func _PersonService_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreatePersonRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(PersonServiceServer).Sayhello(ctx, in)
+		return srv.(PersonServiceServer).Create(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: PersonService_Sayhello_FullMethodName,
+		FullMethod: PersonService_Create_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PersonServiceServer).Sayhello(ctx, req.(*HelloReq))
+		return srv.(PersonServiceServer).Create(ctx, req.(*CreatePersonRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _PersonService_Read_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SinglePersonRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PersonServiceServer).Read(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: PersonService_Read_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PersonServiceServer).Read(ctx, req.(*SinglePersonRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _PersonService_Update_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdatePersonRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PersonServiceServer).Update(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: PersonService_Update_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PersonServiceServer).Update(ctx, req.(*UpdatePersonRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _PersonService_Delete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SinglePersonRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PersonServiceServer).Delete(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: PersonService_Delete_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PersonServiceServer).Delete(ctx, req.(*SinglePersonRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -115,8 +217,20 @@ var PersonService_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*PersonServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Sayhello",
-			Handler:    _PersonService_Sayhello_Handler,
+			MethodName: "Create",
+			Handler:    _PersonService_Create_Handler,
+		},
+		{
+			MethodName: "Read",
+			Handler:    _PersonService_Read_Handler,
+		},
+		{
+			MethodName: "Update",
+			Handler:    _PersonService_Update_Handler,
+		},
+		{
+			MethodName: "Delete",
+			Handler:    _PersonService_Delete_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
